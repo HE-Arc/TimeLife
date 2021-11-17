3f6ae85e573ae392609659de47ac16cb5862da6f
Alexia Montavon <alexia@montavon.xyz>
2021-11-17 10:00:38 +0100

    feat: create Inertia+Vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,24 @@
 require('./bootstrap');
+
+// Import modules...
+import Vue from 'vue';
+import { createInertiaApp } from '@inertiajs/inertia-vue';
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+//import { InertiaProgress } from '@inertiajs/progress';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+Vue.mixin({ methods: { route } });
+Vue.use(Buefy);
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: name => require(`./Pages/${name}`),
+    setup({ el, App, props }) {
+        new Vue({
+            render: h => h(App, props),
+        }).$mount(el)
+    },
+});
+//InertiaProgress.init({ color: '#4B5563' });
