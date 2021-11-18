0c17210ac9118914b56a9afcdba6132cd876fecc
Massimo Ferro <massimo.ferro@he-arc.ch>
2021-11-18 16:26:19 +0100

    feat(#14): create Navbar and Footer

---
+++ resources/js/Component/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <footer class="footer">
+    <div class="content has-text-centered">
+        <p>
+        <strong>TimeLife</strong> by Corentin Faivre, Massimo Ferro and Alexia Montavon. Copyright He-Arc Ingénierie 2021
+        </p>
+    </div>
+    </footer>
+</template>
