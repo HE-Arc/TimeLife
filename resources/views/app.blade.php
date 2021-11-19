ff61dd6b3d142e72d4f81f048081cb48c966dd62
Alexia Montavon <alexia@montavon.xyz>
2021-11-19 10:25:05 +0100

    chore: Remove unused font and js script in blade

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,15 +7,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
-
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Scripts -->
         @routes
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="bg-light">
