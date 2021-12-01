c3de161de55053201f20675c6fef2ad8f72f23ae
Alexia Montavon <alexia@montavon.xyz>
2021-12-01 23:18:49 +0100

    feat(#15): Create basic modal for upload

--- resources/js/Component/UploadModal.vue
+++ resources/js/Component/UploadModal.vue
@@ -1,13 +1,68 @@
 <template>
-$END$
+<div>
+    <div class="modal-card" style="width: auto">
+        <section class="modal-card-body">
+            <b-field>
+                <b-upload v-model="dropFiles"
+                          multiple
+                          drag-drop>
+                    <section class="section">
+                        <div class="content has-text-centered">
+                            <p>
+                                <b-icon
+                                    icon="upload"
+                                    size="is-large">
+                                </b-icon>
+                            </p>
+                            <p>Drop your images here or click to upload (*.jpg, *.jpeg, *.png)</p>
+                        </div>
+                    </section>
+                </b-upload>
+            </b-field>
+
+            <div class="tags">
+                <span v-for="(file, index) in dropFiles"
+                      :key="index"
+                      class="tag is-primary" >
+                    {{file.name}}
+                    <button class="delete is-small"
+                            type="button"
+                            @click="deleteDropFile(index)">
+                    </button>
+                </span>
+            </div>
+        </section>
+        <footer class="modal-card-foot">
+            <b-button
+                label="Close"
+                @click="$emit('close')" />
+            <b-button
+                label="Upload"
+                type="is-primary" />
+        </footer>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-name: "UploadModal"
+    name: "UploadModal",
+    data() {
+        return {
+            dropFiles: []
+        }
+    },
+    methods: {
+        deleteDropFile(index) {
+            this.dropFiles.splice(index, 1)
+        }
+    }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+    .modal-card-body {
+        border-top-left-radius: 6px;
+        border-top-right-radius: 6px;
+    }
 </style>
--- resources/js/Pages/Upload.vue
+++ resources/js/Pages/Upload.vue
@@ -2,6 +2,13 @@
     <div>
         <Head title="Upload pictures"></Head>
         <Menu></Menu>
+        <section>
+            <b-button
+                label="Upload pictures"
+                type="is-primary"
+                size="is-medium"
+                @click="cardModal" />
+        </section>
         <Footer></Footer>
     </div>
 </template>
@@ -10,6 +17,7 @@
 import {Head, Link} from "@inertiajs/inertia-vue";
 import Menu from "../Component/Menu";
 import Footer from "../Component/Footer";
+import UploadModal from "@/Component/UploadModal";
 
 export default {
     name: "Upload",
@@ -18,6 +26,17 @@ export default {
         Menu,
         Footer
     },
+    methods: {
+        cardModal() {
+            this.$buefy.modal.open({
+                parent: this,
+                component: UploadModal,
+                hasModalCard: true,
+                customClass: '',
+                trapFocus: true
+            })
+        }
+    }
 }
 </script>
 
