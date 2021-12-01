331d343768893efadf9578fbc5ee6b9bb288e3a7
Alexia Montavon <alexia@montavon.xyz>
2021-12-01 19:22:26 +0100

    feat(#15): Create empty Upload component

---
+++ resources/js/Pages/Upload.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Head title="Upload pictures"></Head>
+        <Menu></Menu>
+        <Footer></Footer>
+    </div>
+</template>
+
+<script>
+import {Head, Link} from "@inertiajs/inertia-vue";
+import Menu from "../Component/Menu";
+import Footer from "../Component/Footer";
+
+export default {
+    name: "Upload",
+    components: {
+        Head,
+        Menu,
+        Footer
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
