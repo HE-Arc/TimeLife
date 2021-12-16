b02d45a914b925925744130c503ae927b3a0d021
Massimo Ferro <massimo.ferro@he-arc.ch>
2021-12-16 23:10:45 +0100

    feat(#35): add titles and button

---
+++ resources/js/Pages/Album.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <Menu />
+        <Head title="Album" />
+        <section>
+            <!-- Column for title and add album button -->
+            <div class="columns is-vcentered">
+                <div class="column">
+                    <h1 class="title is-2">My Albums</h1>
+                </div>
+                <div class="column is-narrow">
+                    <b-button
+                        label="Add album"
+                        type="is-primary"
+                        class="is-right"
+                        icon-left="plus"
+                        icon-pack="fa"
+                        @click="openCreateModal"
+                    />
+                </div>
+            </div>
+            <!-- Column for title -->
+            <div class="columns is-vcentered">
+                <div class="column">
+                    <h1 class="title is-2">Shared Albums</h1>
+                </div>
+            </div>
+        </section>
+        <Footer />
+    </div>
+</template>
+<style scoped lang="scss">
+.columns {
+    padding: 2rem;
+}
+</style>
+<script>
+import { Head, Link } from "@inertiajs/inertia-vue";
+import Menu from "../Component/Menu.vue";
+import Footer from "../Component/Footer.vue";
+import CreateModal from "../Component/CreateModal";
+
+export default {
+    name: "Album",
+    components: {
+        Link,
+        Head,
+        Menu,
+        Footer,
+    },
+    methods: {
+        openCreateModal() {
+            this.$buefy.modal.open({
+                parent: this,
+                component: CreateModal,
+                hasModalCard: true,
+                customClass: "",
+                trapFocus: true,
+            });
+        },
+    },
+};
+</script>
