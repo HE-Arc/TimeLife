59f5a5ad994cac5682f8582a0dfe707cec7e7a60
Massimo Ferro <massimo.ferro@he-arc.ch>
2021-12-16 17:38:07 +0100

    chore(#31): change value returned val for visibility

--- resources/js/Component/CreateModal.vue
+++ resources/js/Component/CreateModal.vue
@@ -1,45 +1,50 @@
 <template>
-<div>
-    <div class="modal-card" style="width: auto">
-        <form @submit.prevent="submit">
-            <section class="modal-card-body">
-                
-                <b-field label="Album name">
-                    <b-input v-model="form.albumName" placeholder="Album name" type="text" required></b-input>
-                </b-field>
-                
-                <b-field>
-                    <b-radio-button v-model="form.isPrivate"
-                        native-value="true"
-                        type="is-primary is-light is-outlined">
-                        <b-icon icon="lock" pack="fa"></b-icon>
-                        <span>Private</span>
-                    </b-radio-button>
+    <div>
+        <div class="modal-card" style="width: auto">
+            <form @submit.prevent="submit">
+                <section class="modal-card-body">
+                    <b-field label="Album name">
+                        <b-input
+                            v-model="form.albumName"
+                            placeholder="Album name"
+                            type="text"
+                            required
+                        ></b-input>
+                    </b-field>
 
-                    <b-radio-button v-model="form.visibility"
-                        native-value="Public"
-                        type="is-primary is-light is-outlined"
-                        @input="isCreationAllowed"> 
-                        <b-icon icon="unlock" pack="fa"></b-icon>
-                        <span>Public</span>
-                    </b-radio-button>
-                </b-field>
+                    <b-field>
+                        <b-radio-button
+                            v-model="form.isPrivate"
+                            native-value="true"
+                            type="is-primary is-light is-outlined"
+                        >
+                            <b-icon icon="lock" pack="fa"></b-icon>
+                            <span>Private</span>
+                        </b-radio-button>
 
-            </section>
-            
-            <footer class="modal-card-foot">
-                <b-button
-                    label="Close"
-                    @click="$emit('close')" />
-                <b-button
-                    label="Create"
-                    type="is-primary" 
-                    native-type="submit" 
-                    :disabled="!allowCreation" />
-            </footer>
-        </form>
+                        <b-radio-button
+                            v-model="form.isPrivate"
+                            native-value="false"
+                            type="is-primary is-light is-outlined"
+                        >
+                            <b-icon icon="unlock" pack="fa"></b-icon>
+                            <span>Public</span>
+                        </b-radio-button>
+                    </b-field>
+                </section>
+
+                <footer class="modal-card-foot">
+                    <b-button label="Close" @click="$emit('close')" />
+                    <b-button
+                        label="Create"
+                        type="is-primary"
+                        native-type="submit"
+                        :disabled="!allowCreation"
+                    />
+                </footer>
+            </form>
+        </div>
     </div>
-</div>
 </template>
 
 <script>
@@ -48,27 +53,27 @@ export default {
     data() {
         return {
             form: {
-                albumName: this.albumName,
-                visibility: this.visibility
-            }
-        }
+                albumName: "",
+                isPrivate: null,
+            },
+        };
     },
     computed: {
-        allowCreation: function() {
-            return (this.form.albumName && this.form.visibility);
-        }
+        allowCreation() {
+            return this.form.albumName && this.form.isPrivate !== null;
+        },
     },
     methods: {
         submit() {
-            this.$inertia.post(route("albums.store"), this.form)
-        }
-    }
-}
+            this.$inertia.post(route("albums.store"), this.form);
+        },
+    },
+};
 </script>
 
 <style scoped lang="scss">
-    .modal-card-body {
-        border-top-left-radius: 6px;
-        border-top-right-radius: 6px;
-    }
+.modal-card-body {
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
+}
 </style>
