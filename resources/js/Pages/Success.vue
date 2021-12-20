a6abbfdd2f44367e1c2130ee51e0e1ed190e1780
Corentin Faivre <corentin.faivre@he-arc.ch>
2021-12-20 16:11:20 +0100

    fix: add css in success view

--- resources/js/Pages/Success.vue
+++ resources/js/Pages/Success.vue
@@ -10,15 +10,21 @@
                     You can login with your email and password
         </b-message>
         <div class="buttons">
-                <a class="button is-light" href="/login">
-                    Log in
+                <a class="button is-primary" href="/login">
+                    <strong>Log in</strong>
                 </a>
         </div>
     </section>
    <Footer />
     </div>
 </template>
-
+<style scoped>
+ section {
+     width: 40vw;
+     padding: 2rem;
+     margin: 0 auto;
+ }
+</style>
 <script>
 import { Head, Link } from "@inertiajs/inertia-vue";
 import Menu from '../Component/Menu.vue';
