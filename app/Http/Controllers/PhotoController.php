c97e2fb8c0b8b08cfabf51d2ea550a71122ff2fc
Alexia Montavon <alexia@montavon.xyz>
2021-12-28 22:22:55 +0100

    feat(#26): Store picture on disk and in database

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Photo;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class PhotoController extends Controller
@@ -21,7 +22,30 @@ class PhotoController extends Controller
             "images.*" => "image|mimes:jpg,png,jpeg|max:3000"
         ]);
 
-        return response()->json(["error" => "tes2"]);
+        $files = $request->file('images');
+
+        $photosRows = array();
+
+        foreach($files as $file) {
+            $savedPath = $file->store("{$id}", 'album_data');
+
+            // TODO: EXIF DATA EXTRACTION
+
+            array_push($photosRows, [
+               "id_album" => $id,
+               "name" =>  $file->getClientOriginalName(),
+                "description" => "",
+                "filename" => $savedPath,
+                "latitude" => "",
+                "longitude" => "",
+                "date_p" => new \DateTime("@0"), // 1 January 1970
+                "created_at" => new \DateTime(), // Theses two must be specified because Photo::insert doesn't use Eloquent
+                "updated_at" => new \DateTime(), // Theses two must be specified because Photo::insert doesn't use Eloquent
+            ]);
+        }
+
+        // The insertion is done like that so we can let the database deal with the bulk insertion.
+        Photo::insert($photosRows);
 
         return Redirect::route("album.gallery", ["id" => $id]);
     }
