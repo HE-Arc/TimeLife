de8eda7aec5f713a033292e7752a904655eb0eb8
Alexia Montavon <alexia@montavon.xyz>
2021-12-28 22:19:18 +0100

    feat(#26): Up the image size limit to 3000kb

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -15,18 +15,14 @@ class PhotoController extends Controller
 
     public function store(Request $request, int $id)
     {
-        return response()->json(["error" => "test"]);
-
+        // Data validation. A redirect is automatically done on invalid data.
         $request->validate([
             "images" => "required",
-            "images.*" => "image|mimes:jpg,png,jpeg|max:10"
+            "images.*" => "image|mimes:jpg,png,jpeg|max:3000"
         ]);
 
         return response()->json(["error" => "tes2"]);
 
-        $filenameWithExt = $request->file('image')->getClientOriginalName();// Get Filename
-        $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);// Get just Extension
-        $extension = $request->file('image')->getClientOriginalExtension();// Filename To store
-        $fileNameToStore = $filename. '_'. time().'.'.$extension;// Upload Image$path = $request->file(‘image’)->storeAs(‘public/image’, $fileNameToStore);
+        return Redirect::route("album.gallery", ["id" => $id]);
     }
 }
