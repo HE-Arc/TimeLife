30a71d063879d176ec7b8f3825e82f69247c622e
Alexia Montavon <alexia@montavon.xyz>
2022-01-12 02:32:24 +0100

    feat(#62): Add protected route to auth middleware

--- routes/web.php
+++ routes/web.php
@@ -20,31 +20,28 @@ use App\Http\Controllers\AlbumController;
 |
 */
 
+// Unrestricted route
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-        'user' => Auth::user()
-    ]);
+    return Inertia::render('Home');
 })->name('home');
 
-Route::resource('/users', UserController::class)->except(['create']);
-Route::get('/join', [UserController::class, 'create'])->name('users.create');
-Route::get('/profile/{id}', [UserController::class, 'profile'])->name('profile');
-
-Route::resource('/albums', AlbumController::class);
-Route::resource('/albums/{id}/photos', PhotoController::class)->except(['show', 'index']);
-Route::get('/albums/{id}/gallery', [AlbumController::class, 'gallery'])->name("albums.gallery");
-Route::get('/albums/{id}/timeline', [AlbumController::class, 'timeline'])->name("albums.timeline");
-Route::get('/albums/data/{path}', StorageController::class)->where('path', '.*')->name("storage.url");
-
 Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
-
+Route::get('/join', [UserController::class, 'create'])->name('users.create');
 Route::post('/loginCheck', [UserController::class, 'loginCheck'])->name('loginCheck');
 
-Route::get('/createAlbum', [AlbumController::class, 'create']);
+// Restricted routes
+Route::middleware(["auth"])->group(function () {
+    Route::resource('/users', UserController::class)->except(['create']);
 
+    Route::get('/profile/{id}', [UserController::class, 'profile'])->name('profile');
 
+    Route::resource('/albums', AlbumController::class);
+    Route::resource('/albums/{id}/photos', PhotoController::class)->except(['show', 'index']);
+    Route::get('/albums/{id}/gallery', [AlbumController::class, 'gallery'])->name("albums.gallery");
+    Route::get('/albums/{id}/timeline', [AlbumController::class, 'timeline'])->name("albums.timeline");
+    Route::get('/albums/data/{path}', StorageController::class)->where('path', '.*')->name("storage.url");
+
+    Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+
+    Route::get('/createAlbum', [AlbumController::class, 'create']);
+});
